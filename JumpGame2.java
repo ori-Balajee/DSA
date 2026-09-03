b317d80df9b262ddf098f3aa8a9f42bfc11093ca
ori-Balajee <balajee4drip@gmail.com>
2026-09-03 11:05:01 +0530

    JumpGame

---
+++ JumpGame2.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+public class JumpGame2{
+    public static void main(String[] args) {
+
+        int[] arr = {2,3,1,2,5,2,4,8,1,3,2};
+        int[] ans = sol1(arr);
+        System.out.println(Arrays.toString(ans));
+    }
+
+    static int[] sol1(int[] arr){
+        return new int[]{2,3,4,5};
+    }
+    
+}
