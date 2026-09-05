da4a7d9b23bd86801213929c161588decf2c886e
ori-Balajee <balajee4drip@gmail.com>
2026-09-05 16:36:02 +0530

    Trapping rain water

---
+++ TrappingRainWater.java
@@ -0,0 +1,37 @@
+public class TrappingRainWater {
+
+    public static int trap(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = 0;
+        int rightMax = 0;
+        int total = 0;
+
+        while (left <= right) {
+            if (height[left] <= height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    total += leftMax - height[left];
+                }
+                left++;
+
+            } else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    total += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+        return total;
+    }
+
+    public static void main(String[] args) {
+        int[] height = {4, 2, 0, 3, 2, 5};
+        int result = trap(height);
+
+        System.out.println("Trapped water = " + result);
+    }
+}
